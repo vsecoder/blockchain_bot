5fbe5ed924688723d2ccf5f30d52380273c2d401
Всеволод <kreepmeister@gmail.com>
2024-05-21 16:26:51 +0300

    new stuff command

--- app/handlers/owner/stuff.py
+++ app/handlers/owner/stuff.py
@@ -1,10 +1,12 @@
 import time
 
-from aiogram import Router
+from aiogram import Router, Bot
 from aiogram.filters import Command
 from aiogram.types import Message
 
 from app.filters.is_owner import IsOwner
+from app.db.functions import User
+from app.sdk.main import API
 
 router = Router()
 
@@ -18,3 +20,26 @@ async def ping_handler(message: Message):
     await reply_message.edit_text(
         f"<b>⏱ Ping -</b> <code>{round(ping, 3)}</code> <b>ms</b>"
     )
+
+
+@router.message(IsOwner(is_owner=True), Command(commands=["recreate_wallets"]))
+async def recreate_handler(message: Message, bot: Bot):
+    api = API()
+    users = await User.get_all()
+
+    for user in users:
+        wallet = await api.create_wallet()
+        await User.edit_wallet(
+            user.telegram_id, wallet["address"]["pbc"], wallet["address"]["pve"]
+        )
+        api.credit(wallet["address"]["pbc"], 0.01)
+
+        try:
+            await bot.send_message(
+                user.telegram_id,
+                f"💰 <b>В связи с большой обновой кошельки пересозданы, и всем было начислено 0.01 minicoin",
+            )
+        except Exception as e:
+            pass
+
+    await message.answer("🔄 <b>Кошельки пересозданы!</b>")
